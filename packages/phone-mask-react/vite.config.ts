6cc178994e661631d9055ca4d817fd5fb036ebcd
Stefan Popov <stef4n.popov@gmail.com>
2026-02-15 15:55:35 +0100

    feat(react): Improve tree shaking by removing required styles import

--- packages/phone-mask-react/vite.config.ts
+++ packages/phone-mask-react/vite.config.ts
@@ -4,10 +4,16 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+        style: resolve(__dirname, 'src/style.scss')
+      },
       name: 'PhoneMaskReact',
       formats: ['es', 'cjs'],
-      fileName: (format) => {
+      fileName: (format, entryName) => {
+        if (entryName === 'style') {
+          return 'style.css';
+        }
         if (format === 'es') return 'esm/index.js';
         return 'phone-mask-react.cjs.js';
       }
@@ -19,8 +25,7 @@ export default defineConfig({
           react: 'React',
           'react-dom': 'ReactDOM',
           'react/jsx-runtime': 'jsxRuntime'
-        },
-        assetFileNames: 'style.[ext]'
+        }
       }
     },
     sourcemap: true,
