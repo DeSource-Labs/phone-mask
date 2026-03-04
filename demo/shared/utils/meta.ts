5fb944c53452a53b04bd47ce2f19c1e6099a1057
Stefan Popov <stef4n.popov@gmail.com>
2026-03-04 02:23:13 +0100

    feat(demo): Improve meta

--- demo/shared/utils/meta.ts
+++ demo/shared/utils/meta.ts
@@ -3,9 +3,9 @@ export const meta = {
   domain: 'desource-labs.org',
   title: 'Phone Mask: Extreme Small & Fast | DeSource Labs',
   description:
-    'An extremely small, fast, and easy-to-use international phone mask library for Vue, Nuxt, React and JavaScript/TypeScript projects. Supports 250+ countries with localization.',
+    'An extremely small, fast, and easy-to-use international phone mask library for Vue, Nuxt, React, Svelte and JavaScript/TypeScript projects. Supports 250+ countries with localization.',
   keywords:
-    'phone mask, international phone mask, vue phone mask, javascript phone mask, typescript phone mask, intl phone input, phone input library, nuxt phone mask, react phone mask, phone mask library, desource labs, desourcelabs',
+    'phone mask, international phone mask, vue phone mask, javascript phone mask, typescript phone mask, intl phone input, phone input library, nuxt phone mask, svelte phone mask, react phone mask, phone mask library, desource labs, desourcelabs',
   ogImage: 'https://phonemask.desource-labs.org/og.jpg',
   ogImageWidth: '1200',
   ogImageHeight: '630',
