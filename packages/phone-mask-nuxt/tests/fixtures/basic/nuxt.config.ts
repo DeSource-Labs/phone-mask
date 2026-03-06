2eb639ce379a9c04765152bc1f2b5012e153e5aa
Stefan Popov <stef4n.popov@gmail.com>
2026-03-06 02:12:26 +0100

    test(nuxt): Add fixtures

---
+++ packages/phone-mask-nuxt/tests/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt/config';
+import phoneMask from '../../../src/module';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-01-01',
+  modules: [phoneMask]
+});
