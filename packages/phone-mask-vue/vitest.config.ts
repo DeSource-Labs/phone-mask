31782e4c10e4e72223d9fcd5d6707bda1c7a1cf0
Stefan Popov <stef4n.popov@gmail.com>
2026-03-09 23:24:37 +0100

    refactor: Use preferable nodejs imports for build & test scripts

--- packages/phone-mask-vue/vitest.config.ts
+++ packages/phone-mask-vue/vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [vue()],
