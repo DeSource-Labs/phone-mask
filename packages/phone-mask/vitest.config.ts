6be8f483702a90a36c1ab98e518676b0067d5bd7
Stefan Popov <stef4n.popov@gmail.com>
2026-03-13 15:25:50 +0100

    test(core): Improve core test config

--- packages/phone-mask/vitest.config.ts
+++ packages/phone-mask/vitest.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    include: ['tests/unit/**/*.{test,spec}.ts']
+    include: ['tests/unit/**/*.{test,spec}.ts'],
+    coverage: {
+      include: ['src/**/*.{ts,tsx,js,mjs,cjs}'],
+      exclude: ['src/**/*.d.ts']
+    }
   }
 });
